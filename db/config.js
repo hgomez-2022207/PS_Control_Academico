7d17bbf6d1ff216352514aa1963416ee3a57a141
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-02-13 14:38:40 -0600

    Se crea la conexion a la base de datos

---
+++ db/config.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const dbConection = async () => {
+    try{
+        await mongoose.connect(process.env.MONGODB_CNN, {});
+        console.log('Base de datos conectada');
+    }catch(e){
+        throw new Error('Conexion fallida a la base de datos', e)
+    }
+}
+
+module.export = {
+    dbConection
+}
